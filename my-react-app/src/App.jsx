5919dd337972caaa4355211b22741956f174539b
mtzhao0827 <justting_0827@outlook.com>
2025-04-08 15:10:25 +0800

    add count
    Signed-off-by: mtzhao0827 <justting_0827@outlook.com>

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -5,30 +5,20 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [text, setText] = useState('Hello')
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>{text}</h1>
+      <p>当前计数：{count}</p>
+      <button onClick={() => setCount(count + 1)}>增加</button>
+      <button onClick={() => setCount(count - 1)}>减少</button>
+      <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+      />
+    </div>
   )
 }
 
